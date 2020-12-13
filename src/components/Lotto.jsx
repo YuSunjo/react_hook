25bb082804e56d959e59ba4760c49d18f4e3cba4
yusunjo <tnswh2023@gmail.com>
2020-12-13 22:39:35 +0900

    useMemo, useCallback

--- src/components/Lotto.jsx
+++ src/components/Lotto.jsx
@@ -1,4 +1,4 @@
-import React ,{ useRef, useState , useEffect }from 'react'
+import React ,{ useRef, useState , useEffect , useMemo, useCallback}from 'react'
 import LottoBall from './LottoBall'
 
 function getWinNumbers() {
@@ -14,8 +14,11 @@ function getWinNumbers() {
 }
 
 const Lotto=() =>{
-
-    const [WinNumbers, setWinNumbers] = useState(getWinNumbers());
+    //없으면 getWinnumbers()함수가 계속 실행됨  useMemo로 이것을 잠시 기억해 둘 수가 있음 
+    //두번째인자[] 가 바뀌지 않는한 다시 실행되지 않음
+    //[]안에 WinBalls를 넣어주면 useEffect에서 계속 state가 바뀌어서 함수를 계속 실행해줌 
+    const lottoNumbers = useMemo( () => getWinNumbers() , []);
+    const [WinNumbers, setWinNumbers] = useState(lottoNumbers);
     const [WinBalls, setWinBalls] = useState([]);
     const [Bonus, setBonus] = useState(null);
     const [Redo, setRedo] = useState(false);
@@ -38,20 +41,26 @@ const Lotto=() =>{
                 clearInterval(v);
             })
         }
-    },[timeouts.current])   //빈 배열이면 componentDidMount하고 동일
+    },[timeouts.current]);   //빈 배열이면 componentDidMount하고 동일
                     //배열에 요소가 있으면 componentDidmount,ComponentDidUpdate 둘 다 수행
                     // 배열에 winNumber.length===0 을 넣어주면 처음에 두번 실행됨
                     //timeouts.current =[] 이럴 때는 바뀌는것 
                     //timeouts.current[i] = setTimeout이럴때는 요소에 넣어줘서 바뀌는 것이 아님
 
-    const onClickRedo = () => {
-        console.log('onclickRedo')
+                    //useMemo는 값을 기억
+                    //useCallback은 함수 자체를 기억
+                    //[]에 값을 넣어주지 않으면 계속 바뀌지가 않음 => []에 바꾸어야 하는 값을 넣어준다.
+                    //useCallback에 state값을 넣어줄 때 조심...
+                    //자식에게 넘길 때는 useCallback을 꼭 해줘야 한다.
+    const onClickRedo = useCallback(() => {
+        console.log('onclickRedo');
+        console.log(WinNumbers);
         setWinNumbers(getWinNumbers());
         setWinBalls([]);
         setBonus(null);
         setRedo(false);
         timeouts.current = [];
-    }
+    }, [WinNumbers]);
 
     return (
         <div>
