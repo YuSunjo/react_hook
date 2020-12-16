f426428c9bd964b2c8cfb3f182f1e6107ace2b06
yusunjo <tnswh2023@gmail.com>
2020-12-16 22:12:15 +0900

    memo 적용

--- src/components/TicTT/Table.jsx
+++ src/components/TicTT/Table.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, {memo} from 'react'
 import Tr from './Tr'
 
-function Table({ tableData,dispatch}) {
+const Table= memo(({ tableData,dispatch})=> {
     return (
         <>
             <table>
-                {Array(tableData.length).fill().map((tr,i)=> (<Tr 
+                {Array(tableData.length).fill().map((tr,i)=> (<Tr key={i}
                 dispatch={dispatch} rowIndex={i} rowData={tableData[i]}/>))}   
             </table>
         </>
     )
-}
+})
 
 export default Table
